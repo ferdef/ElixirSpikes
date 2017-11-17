c142ade3177c64e1688d8364f4e5661c9c52c502
FdF <theopulus@gmail.com>
2017-11-17 17:01:22 +0100

    Creating the files

--- lib/m3u_creator.ex
+++ lib/m3u_creator.ex
@@ -2,8 +2,9 @@ defmodule M3uCreator do
   @moduledoc """
   Documentation for M3uCreator.
   """
-
   import M3uCreator.CLI, only: [ parse_args: 1 ]
+  import M3uCreator.Browser, only: [ ls_r: 1, ls_folder: 1 ]
+  import M3uCreator.Writer, only: [ write: 2 ]
 
   @doc """
   App entry point.
@@ -23,6 +24,11 @@ defmodule M3uCreator do
   end
 
   defp process(path) do
-    
+    path
+    |> ls_folder
+    |> Enum.each(fn folder->
+      IO.puts("Processing #{folder}")
+      ls_r(folder) |> write(folder <> ".m3u")
+    end)
   end
 end
