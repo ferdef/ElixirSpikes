0cc81f06a7fa6ded0965e210b820d30b4c28b7d9
FdF <theopulus@gmail.com>
2017-11-17 17:13:49 +0100

    wrtier was not being uploaded because of gitignore

---
+++ lib/m3u_creator/writer.ex
@@ -0,0 +1,14 @@
+defmodule M3uCreator.Writer do
+  @moduledoc """
+  File writing related stuff
+  """
+
+  @doc """
+    write a list of filepaths to a text file
+  """
+  def write(items_list, destination, extension) do
+    file = File.open!(destination <> extension, [:write, :utf8])
+    items_list   
+    |> Enum.each(&IO.write(file, String.replace_prefix(&1, destination, "") <> "\n"))
+  end
+end
